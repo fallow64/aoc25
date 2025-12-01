52812e846d69163e0c3c1fbfcd02e70c24bfbdd2
fallow64 <fallow640@gmail.com>
2025-12-01 00:15:05 -0600

    Initial commit, day one

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+mod day1;
+
+use std::{env, fs, process};
+
+const DAY_FUNCS: &[fn(Vec<String>)] = &[day1::run];
+
+fn main() {
+    let mut args = env::args();
+    let program_name = args.next().expect("program name");
+
+    // get the day index
+    let index = match args.next() {
+        Some(index) => match index.parse::<usize>() {
+            Ok(index) => index,
+            Err(_) => {
+                eprintln!("Error: day must be a number");
+                process::exit(1);
+            }
+        },
+        None => {
+            eprintln!("Usage: {program_name} <day> <input_files>");
+            process::exit(1);
+        }
+    };
+
+    let file_names = args.collect::<Vec<String>>();
+
+    let files = file_names
+        .iter()
+        .map(|file_name| {
+            fs::read_to_string(file_name).unwrap_or_else(|_| {
+                eprintln!("Error: could not read file '{file_name}'");
+                process::exit(1);
+            })
+        })
+        .collect::<Vec<String>>();
+
+    if let Some(day_func) = DAY_FUNCS.get(index.wrapping_sub(1)) {
+        day_func(files);
+    } else {
+        eprintln!("Error: day {index} is not implemented");
+        process::exit(1);
+    }
+}
