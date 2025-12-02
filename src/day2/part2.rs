8b5d5a7a8a39664e972a3e5b6531dec5e8058f98
fallow64 <fallow640@gmail.com>
2025-12-02 02:13:55 -0600

    Test update

--- src/day2/part2.rs
+++ src/day2/part2.rs
@@ -1,6 +1,6 @@
 // count invalid
 // where invalid is if a substring is repeated _at all_
-// i.e. 999 = 9 repeated 3 times, 824824824 = 824 repeated 3 times
+// i.e. 999 is 9 repeated 3 times, 824824824 is 824 repeated 3 times
 pub fn solve(input: &[(usize, usize)]) -> usize {
     input
         .iter()
@@ -11,11 +11,13 @@ pub fn solve(input: &[(usize, usize)]) -> usize {
 
 fn has_repeated_substring(s: &str) -> bool {
     let len = s.len();
-    
+
     (1..=len / 2)
         .filter(|&substr_len| len % substr_len == 0)
         .any(|substr_len| {
             let pattern = &s[..substr_len];
-            s.as_bytes().chunks(substr_len).all(|chunk| chunk == pattern.as_bytes())
+            s.as_bytes()
+                .chunks(substr_len)
+                .all(|chunk| chunk == pattern.as_bytes())
         })
 }
