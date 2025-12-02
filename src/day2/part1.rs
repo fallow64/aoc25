55423a195f749428a076da3fa85f0cc6415abe56
fallow64 <fallow640@gmail.com>
2025-12-02 02:23:55 -0600

    Add comments

--- src/day2/part1.rs
+++ src/day2/part1.rs
@@ -3,11 +3,14 @@
 pub fn solve(input: &[(usize, usize)]) -> usize {
     input
         .iter()
+        // transform &[20-25, 30-32] to [20,21,22,23,24,25,30,31,32]
         .flat_map(|(start, end)| *start..=*end)
+        // filter out valid numbers
         .filter(|&num| {
             let num_str = num.to_string();
             let midpoint = num_str.len() / 2;
             num_str.len() % 2 == 0 && &num_str[..midpoint] == &num_str[midpoint..]
         })
+        // sum the invalid numbers
         .sum()
 }
--- src/day2/part2.rs
+++ src/day2/part2.rs
@@ -4,8 +4,11 @@
 pub fn solve(input: &[(usize, usize)]) -> usize {
     input
         .iter()
+        // transform &[20-25, 30-32] to [20,21,22,23,24,25,30,31,32]
         .flat_map(|(start, end)| *start..=*end)
+        // filter out valid numbers
         .filter(|&num| has_repeated_substring(&num.to_string()))
+        // sum the invalid numbers
         .sum()
 }
 
@@ -13,8 +16,10 @@ fn has_repeated_substring(s: &str) -> bool {
     let len = s.len();
 
     (1..=len / 2)
+        // filter out lengths that don't divide evenly
         .filter(|&substr_len| len % substr_len == 0)
         .any(|substr_len| {
+            // check if all chunks equal the first chunk
             let pattern = &s[..substr_len];
             s.as_bytes()
                 .chunks(substr_len)
